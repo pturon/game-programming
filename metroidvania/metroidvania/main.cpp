2a1f7753a65e7fb339db8a8e99b1305b9e03194e
Kalli <pturon@fh-bielefeld.de>
2021-04-22 17:23:50 +0200

    add frame limitation

--- metroidvania/metroidvania/main.cpp
+++ metroidvania/metroidvania/main.cpp
@@ -5,17 +5,32 @@
 const int WINDOW_WIDTH = 600; 
 const int WINDOW_HEIGHT = 400; 
 
+const int FPS = 60;
+const int frameDelay = 1000 / FPS;
+
 Game* game = nullptr;
 
 int main(int argc, char* argv[]) {
+
+	Uint32 frameStart;
+	int frameTime; 
 
 	game = new Game(); 
 	game->init("Metroidvania", WINDOW_WIDTH, WINDOW_HEIGHT, false);
 
 	while (game->running()) {
+
+		frameStart = SDL_GetTicks();
+
 		game->handleEvents();
 		game->update();
 		game->render();
+
+		frameTime = SDL_GetTicks() - frameStart;
+
+		if (frameDelay > frameTime) {
+			SDL_Delay(frameDelay - frameTime);
+		}
 	}
 	game->clean();
 
