64307bec7e0fee42402c00acf9a12f315302b54d
Kalli <pturon@fh-bielefeld.de>
2021-05-18 11:06:56 +0200

    add  rect rect collision bool

--- metroidvania/metroidvania/include/Collision.h
+++ metroidvania/metroidvania/include/Collision.h
@@ -9,5 +9,7 @@ public:
 	static bool AABB(const SDL_Rect& a, const SDL_Rect& b);
 	static bool AABB(const ColliderComponent& colA, const ColliderComponent& colB);
 	static bool PointRect(const Vector2D& p, const SDL_Rect& r);
+	static bool RectRect(const SDL_Rect& r1, const SDL_Rect& r2);
+	static bool RectRect(const ColliderComponent& r1, const ColliderComponent& r2);
 };
 
--- metroidvania/metroidvania/src/Collision.cpp
+++ metroidvania/metroidvania/src/Collision.cpp
@@ -15,3 +15,11 @@ bool Collision::AABB(const ColliderComponent& colA, const ColliderComponent& col
 bool Collision::PointRect(const Vector2D& p, const SDL_Rect& r) {
 	return (p.x >= r.x && p.y >= r.y && p.x < r.x + r.w && p.y < r.y + r.h);
 }
+
+bool Collision::RectRect(const SDL_Rect& r1, const SDL_Rect& r2) {
+	return (r1.x < r2.x + r2.w && r1.x + r1.w > r2.x && r1.y < r2.y + r2.h && r1.y + r1.h > r2.y);
+}
+
+bool Collision::RectRect(const ColliderComponent& r1, const ColliderComponent& r2) {
+	return RectRect(r1.collider, r1.collider);
+}
