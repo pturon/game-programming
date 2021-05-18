da65a9e1227732662bab31029e3c2b8c8badf9ab
Kalli <pturon@fh-bielefeld.de>
2021-05-18 11:02:32 +0200

    simplified sprite

--- metroidvania/metroidvania/src/Game.cpp
+++ metroidvania/metroidvania/src/Game.cpp
@@ -49,7 +49,7 @@ void Game::init(const char* title, int width, int height, bool fullscreen) {
 
 	player.addComponent<StateComponent>(); 
 	player.addComponent<KeyboardController>();
-	player.addComponent<TransformComponent>((WINDOW_WIDTH-PLAYER_WIDTH)/2, (WINDOW_HEIGHT-PLAYER_HEIGHT) / 2, PLAYER_WIDTH, PLAYER_HEIGHT, 1, 5, true);
+	player.addComponent<TransformComponent>(864, 256, PLAYER_WIDTH, PLAYER_HEIGHT, 1, 5, true);
 	player.addComponent<ColliderComponent>("Player");
 	player.addComponent<SpriteComponent>("assets/hero_spritesheet.png", true);
 	player.addComponent<AttackComponent>();
@@ -79,151 +79,39 @@ void Game::update() {
 	
 	for (auto& c : colliders) {
 		SDL_Rect cCol = c->getComponent<ColliderComponent>().collider;
-		if (Collision::AABB(cCol, playerCol)) {				
+		if (Collision::AABB(cCol, playerCol)) {			
 			
-			//down
-			if (x == 0 && y > 0) {
-				if (cCol.y >= playerCol.y + playerCol.h - y) {
-					if (playerCol.y + playerCol.h > cCol.y && playerCol.y < cCol.y) {
-						playerCol.y = cCol.y - playerCol.h;
-						player.getComponent<TransformComponent>().stopJump();
-					}
-				}
-			}
-			//up
-			else if (x == 0 && y < 0) {
-				if (playerCol.y < cCol.y + cCol.h && playerCol.y + playerCol.h > cCol.y + cCol.h) {
-					playerCol.y = cCol.y + cCol.h;					
-					player.getComponent<TransformComponent>().startFall();
-				}
-			}
-			//right
-			else if (x > 0 && y == 0) {
-				if (playerCol.x + playerCol.w > cCol.x && playerCol.x < cCol.x) {
-					playerCol.x = cCol.x - playerCol.w;
-					if (player.getComponent<StateComponent>().currentState == dashing) {
-						player.getComponent<TransformComponent>().stopDash();
-					}
-					if (player.getComponent<StateComponent>().currentState != walking) {
-						player.getComponent<TransformComponent>().startWallCling(right);
-					}				
-				}
-			}
-			//left
-			else if (x < 0 && y == 0) {
-				if (playerCol.x < cCol.x + cCol.w && playerCol.x + playerCol.h > cCol.x + cCol.w) {
-					playerCol.x = cCol.x - cCol.w;
-					if (player.getComponent<StateComponent>().currentState == dashing) {
-						player.getComponent<TransformComponent>().stopDash();
-					}
-					if (player.getComponent<StateComponent>().currentState != walking) {
-						player.getComponent<TransformComponent>().startWallCling(left);
-					}					
-				}
-			}
-			//rightUp
-			else if (x > 0 && y < 0) {
-						
-			}
-			//leftUp
-			else if (x < 0 && y < 0) {
-								
-			}
-			//rightDown
-			else if (x > 0 && y > 0) {								
-										
-			}
-			//leftDown
-			else if (x < 0 && y > 0) {
-				
-			}
-
-
-			/**
 			//Player is above
-			if (playerCol.y + playerCol.h > cCol.y && playerCol.y < cCol.y) {	
-				playerCol.y = cCol.y - playerCol.h;
+			if (playerCol.y + playerCol.h > cCol.y && playerCol.y < cCol.y) {
 				player.getComponent<TransformComponent>().stopJump();
 			}
 			//Player is below
 			else if (playerCol.y < cCol.y + cCol.h && playerCol.y + playerCol.h > cCol.y + cCol.h) {
-				playerCol.y = cCol.y + cCol.h;
 				player.getComponent<TransformComponent>().startFall();
 			}
 			if (cCol.y < playerCol.y + playerCol.h && cCol.y + cCol.h > playerCol.y) {
 				//Player is right
-				if (playerCol.x + playerCol.w > cCol.x && playerCol.x < cCol.x) {
-					playerCol.x = cCol.x - playerCol.w;
+				if (playerCol.x + playerCol.w > cCol.x && playerCol.x < cCol.x) {					
 					if (player.getComponent<StateComponent>().currentState == dashing) {
 						player.getComponent<TransformComponent>().stopDash();
 					}
 					if (player.getComponent<StateComponent>().currentState != walking) {
 						player.getComponent<TransformComponent>().startWallCling(right);
-					}
-					collisionSide = true;
+					}					
 				}
 				//Player is left
-				else if (playerCol.x < cCol.x + cCol.w && playerCol.x + playerCol.h > cCol.x + cCol.w) {
-					playerCol.x = cCol.x - cCol.w;
+				else if (playerCol.x < cCol.x + cCol.w && playerCol.x + playerCol.h > cCol.x + cCol.w) {					
 					if (player.getComponent<StateComponent>().currentState == dashing) {
 						player.getComponent<TransformComponent>().stopDash();
 					}
 					if (player.getComponent<StateComponent>().currentState != walking) {
 						player.getComponent<TransformComponent>().startWallCling(left);
-					}
-					collisionSide = true;
+					}				
 				}
-			}
-			*/
-
-			/**
-			if (cCol.y < playerCol.y + PLAYER_HEIGHT && playerCol.y < cCol.y + cCol.h) {
-				//right 
-				if (playerCol.x + playerCol.w - cCol.x >= 0 && playerCol.x + playerCol.w - cCol.x < 20) {
-					playerCol.x = cCol.x - PLAYER_WIDTH - 1;	
-					if (player.getComponent<StateComponent>().currentState == dashing) {
-						player.getComponent<TransformComponent>().stopDash();
-					}
-					if (player.getComponent<StateComponent>().currentState != walking) {
-						player.getComponent<TransformComponent>().startWallCling(right);
-					}					
-					collisionSide = true;
-				}
-				//left 
-				if (playerCol.x > cCol.x && playerCol.x < cCol.x + cCol.w && cCol.x + cCol.w - playerCol.x < 20) {
-					playerCol.x = cCol.x + cCol.w + 1;				
-					if (player.getComponent<StateComponent>().currentState == dashing) {
-						player.getComponent<TransformComponent>().stopDash();
-					}
-					if (player.getComponent<StateComponent>().currentState != walking) {
-						player.getComponent<TransformComponent>().startWallCling(left);
-					}					
-					collisionSide = true; 
-				}
-			}
-			if (cCol.x < playerCol.x + PLAYER_WIDTH && cCol.x + cCol.w > playerCol.x && player.getComponent<StateComponent>().currentState != dashing) {
-				//above
-				if (playerCol.y + PLAYER_HEIGHT > cCol.y && playerCol.y + PLAYER_HEIGHT < cCol.y + cCol.h && playerCol.y + PLAYER_HEIGHT - cCol.y <= yDist) {
-					playerCol.y = cCol.y - PLAYER_HEIGHT - 1;
-					player.getComponent<TransformComponent>().stopJump();					
-				}
-				//below 
-				if (playerCol.y > cCol.y && playerCol.y < cCol.y + cCol.h) {
-					playerCol.y = cCol.y + cCol.h + 1;
-					player.getComponent<TransformComponent>().startFall();
-				}
-			}			
-
-			*/
+			}					
 		}
 	}
-	/**
-	if (!collisionSide && player.getComponent<StateComponent>().currentState == wallCling) {
-		player.getComponent<TransformComponent>().stopWallCling();
-	}
-	if (collisionSide && collisionBottom && player.getComponent<StateComponent>().currentState == wallCling) {
-		player.getComponent<TransformComponent>().stopWallCling();
-	} **/
+
 	player.getComponent<TransformComponent>().position.x = playerCol.x;
 	player.getComponent<TransformComponent>().position.y = playerCol.y;
 	player.getComponent<ColliderComponent>().collider.x = playerCol.x;
